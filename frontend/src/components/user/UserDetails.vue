c92da3cf72409614e41dc0d3f8d44c402130d7be
Jonathan Fridja <jonyfridja@gmail.com>
2019-07-11 20:00:27 +0300

    git ignore on actual data and some nonebreaking change

---
+++ frontend/src/components/user/UserDetails.vue
@@ -0,0 +1,41 @@
+<template>
+  <section>
+    <h1>Profile Page</h1>
+    <div>
+        
+    </div>
+    {{ user }}
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        _id: "u101",
+        name: "Salom Anderson",
+        email: "gallridge1d@smh.com.au",
+        phone: "747-910-1663",
+        loc: {
+          geo: {
+            lat: 40.7559095,
+            lng: -0.3371889
+          },
+          address: "1092 Saint Paul Circle",
+          city: "Teruel"
+        }
+      }
+    };
+  },
+  computed: {}
+};
+</script>
+
+
+<style lang="scss" scoped>
+$mlr: #ddd;
+section {
+  color: $mlr;
+}
+</style>
