78295b6c4dfd206bb112520d956fff769f9f72fa
Yarin Bahar <yarinb1@gmail.com>
2019-07-12 14:37:29 +0300

    store modules

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,38 +1,20 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import eventeraService from '@/services/EventeraService'
+import eventeraStore from '@/modules/EventeraStore'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+  strict: true,
+  modules: {
+    eventeraStore
+  },
   state: {
-    eventeras : [],
   },
   mutations: {
-    saveEventera(state, { eventera, _id }) {
-      let idx = state.eventeras.findIndex((currEventera) => currEventera._id === _id)
-      state.eventeras.splice(idx, 1, eventera)
 
-    },
-    setEventeras(state,eventeras){
-      state.eventeras = eventeras;
-    }
   },
   actions: {
-    async saveEventera(context, eventera) {
-      let updatedEventera;
-      if (eventera._id) {
-        updatedEventera = await eventeraService.update(eventera)
-      } else updatedEventera = await eventeraService.add(eventera)
-      context.commit({ type: 'saveEventera', Updatedeventera: updatedEventera, _id: eventera._id })
-      return updatedEventera
-    },
-    async loadEventeras(context,filterBy = null){
-     eventeras = await eventeraService.query(filterBy)
-     context.commit({type:'setEventeras',eventeras})
-    }
+
   },
-  async getEventeraById(context,{_id}){
-      let eventera = await eventeraService.getById(_id);
-      return eventera;
-  }
+
 })
