73098eaaf2db46fd2b78b5082d9ac15f33210c05
Jonathan Fridja <jonyfridja@gmail.com>
2019-07-18 12:53:32 +0300

    preview decimated

--- frontend/src/components/EventeraPreview.vue
+++ frontend/src/components/EventeraPreview.vue
@@ -1,14 +1,19 @@
 <template>
-  <slide>
-    <section class="eventera-preview flex column flex-center">
-      <h2>{{eventera.name}}</h2>
-      <p>
-        <span>{{ eventera.expireAt | moment("dddd, MMMM Do YYYY, h:mm:ss a") }}</span>
-      </p>
-      <p>{{eventera.loc.address}},{{eventera.loc.city}}</p>
-      <router-link class="btn" tag="div" :to="detailsUrl">Details</router-link>
-    </section>
-  </slide>
+  <section class="eventera-preview flex column">
+    <img src="@/imgs/dummy-card.png" alt />
+    <div class="secondary">
+      <span>{{eventera.categories[0] | capitalize }}</span>
+      <span v-if="distance">| {{distance}} away</span>
+    </div>
+
+    <h2>{{eventera.name}}</h2>
+    {{eventera}}
+    <!-- <p>
+      <span>{{ eventera.expireAt | moment("dddd, MMMM Do YYYY, h:mm:ss a") }}</span>
+    </p>
+    <p>{{eventera.loc.address}},{{eventera.loc.city}}</p>
+    <router-link class="btn" tag="div" :to="detailsUrl">Details</router-link>-->
+  </section>
 </template>
 
 <script>
@@ -19,6 +24,11 @@ export default {
   computed: {
     detailsUrl() {
       return `/eventera/${this.eventera._id}`;
+    },
+    distance() {
+      const loc = this.$store.getters.loc;
+      if (!loc) return "";
+      return "0.5 km";
     }
   }
 };
